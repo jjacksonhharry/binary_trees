cfdf5c5ac9c4189d91ee1b3af90bbac75219a455
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-08-30 15:51:32 +0300

    Fix task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,11 +11,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	size_t count = 0;
 
 	if (tree == NULL)
-		return (0);
+		return (count);
 
-	if (tree->left == NULL && tree->right != NULL)
-		count++;
-	else if (tree->left != NULL && tree->right == NULL)
+	if (tree->left != NULL || tree->right != NULL)
 		count++;
 
 	count += binary_tree_nodes(tree->left);
