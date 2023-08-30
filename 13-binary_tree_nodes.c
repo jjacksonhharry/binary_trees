1a2b6293c712ad7231849d0adfc505ce71c49e85
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-08-30 14:46:28 +0300

    Function that counts half nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with atleast 1 child
+ * @tree: root node pointer
+ *
+ * Return: count of half nodes and 0 on failure
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right != NULL)
+	       count++;
+	if (tree->left != NULL && tree->right == NULL)
+		count++;
+
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+	return (count);
+}
